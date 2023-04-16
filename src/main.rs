6ed2d3c5e3621bc0db670fb63eef4aac334053b7
Lars-Kristian Svenoy <lakrsv@gmail.com>
2023-04-16 13:41:33 +0100

    Fill entire window

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@ use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                // fill the entire browser window
+                fit_canvas_to_parent: true,
+                // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
         //.add_startup_system(setup_2d_mesh)
         //.add_startup_system(setup_2d_sprite)
         .add_plugin(MoveSpritePlugin)
