0169fe9277a74fc7abc0af92fb3c5d19399c6330
Lars-Kristian Svenoy <lakrsv@gmail.com>
2023-04-16 12:31:05 +0100

    Add ECS example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
+}
+
+fn hello_world() {
+    println!("Hello world!");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("Hello {}!", name.0);
+    }
 }
